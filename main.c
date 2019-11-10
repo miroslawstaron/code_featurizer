1020f2a7e8b54c4add8d0de8b6f5e8837366551b
Miroslaw Staron <miroslaw@CSE-3536735.net.chalmers.se>
2019-11-10 22:09:20 +0100

    Tested the featurizer on the small code

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// avoid int main(int argc) 
+int main(int argc, char *argv)
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+
+   return 0;
+}
+
+
+
+
+
